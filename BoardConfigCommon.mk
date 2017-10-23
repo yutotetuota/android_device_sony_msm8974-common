3b8a73140033b2e5b3f56c0b8a81f33f60ce5788
Keita <keita_nya@live.jp>
2017-10-23 15:57:05 -0300

    msm8974-common: Move sepolicy to shinano-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,9 +102,6 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/sony/msm8974-common/sepolicy
-
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 
